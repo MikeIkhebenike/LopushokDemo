0facff3a4bece67d8e7e6b53fc2ebca26c711a97
Майкл <mihebenike@gmail.com>
2021-10-22 13:02:20 +0300

    Добавьте файлы проекта.

---
+++ LopushokDemo/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LopushokDemo
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void sortBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void filterBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnUpDown_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LopushokDemo/Product.cs
@@ -0,0 +1,37 @@
+namespace LopushokDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Product")]
+    public partial class Product
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        public int? ProductTypeID { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string ArticleNumber { get; set; }
+
+        public string Description { get; set; }
+
+        [StringLength(100)]
+        public string Image { get; set; }
+
+        public int? ProductionPersonCount { get; set; }
+
+        public int? ProductionWorkshopNumber { get; set; }
+
+        public decimal MinCostForAgent { get; set; }
+
+        public virtual ProductType ProductType { get; set; }
+    }
+}
